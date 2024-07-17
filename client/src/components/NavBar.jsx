457bee98bc8cacf22e0628f14fbb8efdc9ab832f
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-07-17 15:29:39 -0400

    started work on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  Inputbase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+
 function NavBar() {
-  return <div></div>;
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const navigate = useNavigate();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  // For later to add when i figure out how to manage remote data
+  // const fullName = `${firstName} ${lastName}`;
+
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem"></FlexBetween>
+    </FlexBetween>
+  );
 }
 
 export default NavBar;
