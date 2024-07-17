24c4ec81e535bea906d04b2c479c04a0188c54f8
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-07-17 15:15:26 -0400

    created flexbetwen styling added themprovider for material ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+
+import { themeSettings } from "./theme";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
