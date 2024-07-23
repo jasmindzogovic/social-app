6c7687bd0fd29d562c231500a7e471b2fcb5464c
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-07-23 13:21:52 -0400

    added formik to project

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,9 @@
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { FormControl, FormLabel } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useFormik } from "formik";
+
 import {
   signUp,
   logIn,
@@ -9,13 +13,45 @@ import {
 } from "../services/auth";
 
 function LoginPage() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   const {
     status,
     error,
     data: user,
   } = useQuery({ queryKey: ["user"], queryFn: logIn });
 
-  return <div>Login Page</div>;
+  return (
+    <>
+      <FormControl autoComplete="off">
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <TextField
+          size="small"
+          value={formik.values.email}
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <TextField
+          size="small"
+          value={formik.values.password}
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Button>Submit</Button>
+      </FormControl>
+    </>
+  );
 }
 
 export default LoginPage;
--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -6,6 +6,8 @@ export async function logIn() {
   const data = axios
     .get("127.0.0.1:8000/api/v1/users/login")
     .then((res) => res.data);
+
+  console.log(data);
 }
 
 export async function logOut() {}
